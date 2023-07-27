b0e51693aa67ecf54bb28bdd29709fe9501d3401
Lavatsaleo <lavatsaleo@gmail.com>
2023-07-27 16:28:41 +0300

    adddougnutchart

--- src/app/mdashboard/overview/overview.component.ts
+++ src/app/mdashboard/overview/overview.component.ts
@@ -14,13 +14,59 @@ export class OverviewComponent {
     series: [{
       data: [1, 2, 3],
       type: 'pie'
-    }]
+    }],
+    plotOptions: {
+                    pie: {
+                        innerSize: "70%", // Adjust the innerSize to control the size of the inner hole (donut hole)
+                        depth: 25, // Adjust the depth to control the thickness of the donut
+                        dataLabels: {
+                            enabled: false, // Disable data labels inside the donut segments
+                        },
+                    },
+                },
   };
   ageCategories = [
 		"0-4 yrs",
 		"5-9 yrs",
 		"15-34 yrs",
 	];
+    // const PieChart2 = ({h}) => {
+    //     const options = {
+    //         chart: {
+    //             type: "pie",
+    //         },
+    //         plotOptions: {
+    //             pie: {
+    //                 innerSize: "50%", // Adjust the innerSize to control the size of the inner hole (donut hole)
+    //                 depth: 45, // Adjust the depth to control the thickness of the donut
+    //                 dataLabels: {
+    //                     enabled: false, // Disable data labels inside the donut segments
+    //                 },
+    //             },
+    //         },
+    //         title: {
+    //             text: "COVID-19 Positivity",
+    //         },
+    //         colors: [
+    //             "#FF0000",
+    //             "green", 
+    //         ],
+    //         series: [
+    //             {
+    //                 name: "Data",
+    //                 data: [
+    //                     ["Posivite", 20],
+    //                     ["Negative", 30],
+    //                 ], // Replace with your data values
+    //             },
+    //         ],
+    //     };
+    //     const containerProps = {
+    //         style: {
+    //             height: h?? "100%",
+    //         },
+    //     };
+    
 	// positivitybysexandagechartOptions: Highcharts.Options= {
 	// 	chart: { type: "bar" },
 	// 	title: { text: "Participant Distribution by Age, Sex" },
