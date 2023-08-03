1614e8eb5788ff7b58dde63dbdff6f88b2d76e44
Lavatsaleo <lavatsaleo@gmail.com>
2023-08-03 21:08:26 +0300

    screeningcharts

--- src/app/mdashboard/screened/screened.component.ts
+++ src/app/mdashboard/screened/screened.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-screened',
@@ -6,5 +7,220 @@ import { Component } from '@angular/core';
   styleUrls: ['./screened.component.css']
 })
 export class ScreenedComponent {
+    Highcharts: typeof Highcharts = Highcharts;
+    screenedbygenderchartOptions: Highcharts.Options = {
+      title: {
+        text: 'Screnned by Gender',
+        align: 'left'
+    },
 
+    chart: {
+        type: "pie",
+    },
+
+    colors: [
+        "#234FEA", // Color for Category 2
+        "#FC7500", // Color for Category 3
+    ],
+    series: [
+        {
+            name: "Data",
+            type: 'pie',
+            data: [
+                ["Male", 20],
+                ["Female", 30],
+            ], // Replace with your data values
+        },
+    ],
+    
+  };
+
+  screeningbyageandgenderchartOptions: Highcharts.Options = {
+
+    title: {
+      text: 'Screened by age & Gender',
+      align: 'left',
+  },
+
+  chart: { type: "bar" },
+  xAxis: [
+      {
+          categories: ["0-4 yrs", "5-9 yrs", "15-34 yrs"],
+          title: { text: "" },
+          reversed: false
+      },
+      {
+          categories: ["0-4 yrs", "5-9 yrs", "15-34 yrs"],
+          title: { text: "" },
+          reversed: false,
+          linkedTo: 0,
+          opposite: true,
+      },
+  ],
+  yAxis: [
+      {
+          // labels: {
+          //     formatter: function () {
+          //         return Math.abs(parseInt(this.value)).toString();
+          //     },
+          // },
+      },
+  ],
+  plotOptions: { series: { stacking: "normal" }, bar: { pointWidth: 18 } },
+  tooltip: {
+  },
+  legend: { align: "left", verticalAlign: "top", y: 0, x: 80 },
+  series: [
+      {
+          name: "Female",
+          data: [10, 60, 30],
+          color: "#FC7500",
+          type: 'bar'
+      },
+      {
+          name: "Male",
+          data: [-9, -41, -34],
+          color: "#234FEA",
+          type: 'bar'
+      },
+  ],
+
+
+
+
+  };
+
+  screenedbyhealthfacilitieschartOptions: Highcharts.Options = {
+
+    title: {
+      text: 'No screened at different health facilities',
+      align: 'left'
+  },
+  chart: {
+      type: "column",
+  },
+  // title: {
+  // 	text: "Enrollment Cascade",
+  // },
+  xAxis: {
+      categories: ["Kenyatta National Hospital", "Busia County Referral", "Marsabit County ", "JOOTRH", "Makueni"], // Replace with your categories
+  },
+  yAxis: {
+      title: {
+          text: "Number Positive",
+      },
+  },
+
+  series: [
+      {
+          name: "Health Facilities",
+          data: [60, 55, 20, 20, 15],
+          type: 'column',
+          color: "#234FEA",
+      },
+  ],
+
+  
+  };
+
+
+  screenedovertimechartOptions: Highcharts.Options = {
+
+    chart: {
+      //zoomType: 'xy'
+  },
+  title: {
+      text: 'Screened over time',
+      align: 'left'
+  },
+  // subtitle: {
+  //     text: 'Source: ' +
+  //         '<a href="https://www.yr.no/nb/historikk/graf/5-97251/Norge/Troms%20og%20Finnmark/Karasjok/Karasjok?q=2021"' +
+  //         'target="_blank">YR</a>',
+  //     align: 'left'
+  // },
+  xAxis: [{
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      crosshair: true
+  }],
+  yAxis: [{ // Primary yAxis
+      labels: {
+          format: '{value}',
+          // style: {
+          //     color: Highcharts.getOptions().colors[1]
+          // }
+      },
+      title: {
+          text: 'Number Tested',
+          // style: {
+          //     color: Highcharts.getOptions().colors[1]
+          // }
+      }
+  }, { // Secondary yAxis
+      title: {
+          text: 'Tested Positive',
+          // style: {
+          //     color: Highcharts.getOptions().colors[0]
+          // }
+      },
+      labels: {
+          format: '{value}%',
+          // style: {
+          //     color: Highcharts.getOptions().colors[0]
+          // }
+      },
+      opposite: true
+  }],
+  tooltip: {
+      shared: true
+  },
+  legend: {
+      align: 'left',
+      x: 80,
+      verticalAlign: 'top',
+      y: 60,
+      floating: true,
+      // backgroundColor:
+      //     Highcharts.defaultOptions.legend.backgroundColor || // theme
+      //     'rgba(255,255,255,0.25)'
+  },
+  series: [{
+      name: 'Sample Tested',
+      type: 'column',
+      color: '#234FEA',
+      yAxis: 1,
+      data: [27.6, 28.8, 21.7, 34.1, 29.0, 28.4, 45.6, 51.7, 39.0,
+          60.0, 28.6, 32.1],
+      tooltip: {
+          valueSuffix: ' mm'
+      }
+
+  }, {
+      name: '% Positivity',
+      type: 'spline',
+      data: [-13.6, -14.9, -5.8, -0.7, 3.1, 13.0, 14.5, 10.8, 5.8,
+      -0.7, -11.0, -16.4],
+      color: 'red',
+      tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
+      },
+      accessibility: { point: { valueSuffix: '%' } },
+      // valueSuffix: '°C'
+      // tooltip: {
+      //     pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
+      // },
+      // accessibility: { point: { valueSuffix: '%' } },
+
+  }]
+};
+
+
+
+
+
+
+
+  
 }
+
